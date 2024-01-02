858813e3810bb45e5f4f32decf0701c68fc69d63
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-01-02 15:11:57 +0100

    error unit test

---
+++ src/Error.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Error from './Error';
+
+test('renders Error component', () => {
+  render(<Error />);
+  
+  // Assert that the Error component is rendered
+  const errorElement = screen.getByText(/Error/i);
+  expect(errorElement).toBeInTheDocument();
+});
